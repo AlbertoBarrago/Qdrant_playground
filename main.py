868063d0d41a763e13cb6aca961c1cd2ae2d0a8c
alBz <me@albz.dev>
2023-12-13 20:09:27 +0100

    Init commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from service.quadrant_db import connect_quadrant, close_connection, create_vector, create_collection, search_vector
+
+if __name__ == '__main__':
+    search_vector()
---
+++ service/quadrant_db.py
@@ -0,0 +1,64 @@
+import os
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+import numpy as np
+from qdrant_client.models import PointStruct
+from dotenv import load_dotenv
+
+load_dotenv()
+
+qdrant_url = os.getenv('QDRANT_URL')
+qdrant_token = os.getenv('QDRANT_TOKEN')
+
+
+def connect_quadrant():
+    client = QdrantClient(
+        url=qdrant_url,
+        api_key=qdrant_token,
+    )
+    print(client)
+    return client
+
+
+def create_collection():
+    client = connect_quadrant()
+
+    client.recreate_collection(
+        collection_name="albz_collection",
+        vectors_config=VectorParams(size=100, distance=Distance.COSINE),
+    )
+
+
+def close_connection():
+    client = connect_quadrant()
+
+    client.close()
+
+
+def create_vector():
+    client = connect_quadrant()
+
+    vectors = np.random.rand(100, 100)
+    client.upsert(
+        collection_name="albz_collection",
+        points=[
+            PointStruct(
+                id=idx,
+                vector=vector.tolist(),
+                payload={"name": "Bibi", "status": "Bibi's lover"}
+            )
+            for idx, vector in enumerate(vectors)
+        ]
+    )
+
+
+def search_vector():
+    client = connect_quadrant()
+    query_vector = np.random.rand(100)
+    hits = client.search(
+        collection_name="albz_collection",
+        query_vector=query_vector,
+        limit=5  # Return 5 closest points
+    )
+    print(hits)
+    return hits
